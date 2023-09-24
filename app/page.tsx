b73b65ddc124a189927b66dfd131cdc4f935d557
hannahf86 <hannahfeehan.design@gmail.com>
2023-09-24 12:08:12 +0100

    The maths is complete!

--- app/page.tsx
+++ app/page.tsx
@@ -5,35 +5,51 @@ import { useState, useEffect } from "react";
 export default function Home() {
 
   const styles = {
-    background: "bg-blue-300 h-screen w-screen",
-    title: "text-center text-3xl text-white py-8 mx-4 font-bold",
+    background: "bg-blue-300 h-full w-screen",
+    title: "text-center text-3xl text-white py-4 mx-4 font-bold",
 
-    container: "bg-white mx-6 p-6 rounded-lg ",
+    container: "bg-white mx-6 mb-6 p-6 rounded-lg ",
     questionContainer: "flex flex-col justify-center mb-6 shadow-md",
     questionText: "text-lg text-center mb-3 font-bold tracking-wider",
     input: "m-auto px-4 py-2 rounded-lg border-none bg-gray-200 text-center w-5/12 mb-4",
     answer: 'text-right text-teal-700 font-bold text-md my-4 mx-6',
     dropdownMenu: 'px-4 py-2 rounded-lg mb-4 w-9/12 m-auto',
     totalText: 'text-xl font-bold text-center',
-    totalAnswer: 'text-center font-bold text-xl mt-2 mb-6',
+    totalAnswer: 'text-center text-teal-700 font-bold text-xl mt-2 mb-6',
   };
 
   const [childAge, setChildAge] = useState<number>(0);
   const [funding, setFunding] = useState<string>(" ");
   const [hours, setHours] = useState<number>(0);
   const [rate, setRate] = useState<number>(0);
+  const [fundingHours, setFundingHours] = useState<number>(0);
 
   // !! DONE - By wrapping your changes to setRate in a useEffect, you'll only update the rate if
   // !! DONE - your childAge updates, rather than all the time 
   useEffect(() => {
     if (childAge <= 2) {
-      setRate(5.5);
+      setRate(5.50);
     } else if (childAge <= 4) {
-      setRate(5);
+      setRate(5.00);
     } else {
-      setRate(4.5);
+      setRate(4.50);
     }
   }, [childAge]);
+
+  useEffect(() => {
+    if (funding === "2 year old, 15 hours funding") {
+      setFundingHours(15);
+    } else if (funding === "3 year old, 15 hours funding") {
+      setFundingHours(15);
+    } else if (funding === "3 year old, 30 hours funding") {
+      setFundingHours(30);
+    } else {
+      setFundingHours(0);
+    }
+  }, [funding])
+
+  console.log(funding);
+  console.log(setFundingHours);
 
   // You could figure out exactly which cantation you need to get the
   // event typing correct in Typescript, or you can cut to the chase
@@ -55,6 +71,20 @@ export default function Home() {
     setHours(Number(e.target.value));
   };
 
+
+  // THE MATHS.....
+  // 1) Take in the childAge to define the rate - !! DONE
+  // 2) rate * hours = totalHours - !! DONE
+  // 3) totalHours - funding 
+
+  // STEP TWO
+  const totalCost = rate * hours;
+
+  // STEP THREE
+  const totalHours = hours - fundingHours;
+  const totalAmount = fundingHours * rate;
+
+
   return (
     <>
       <div id="calculator" className={styles.background}>
@@ -72,18 +102,7 @@ export default function Home() {
               onChange={handleAge}
               className={styles.input}
             />
-            <div className={styles.answer}>{childAge}</div>
-          </div>
-
-          <div className={styles.questionContainer}>
-            <h2 className={styles.questionText}> Select funding options</h2>
-            <select onChange={handleFunding} className={styles.dropdownMenu}>
-              <option value="2 year old, 15 hours funding">2yrs 15hrs</option>
-              <option value="3 year old, 15 hours funding">3yrs 15hrs</option>
-              <option value="3 year old, 30 hours funding">3yrs 30hrs</option>
-              <option value="No funding">NONE</option>
-            </select>
-            <div className={styles.answer}>{funding}</div>
+            <div className={styles.answer}>£{rate} per hour</div>
           </div>
 
           <div className={styles.questionContainer}>
@@ -96,13 +115,30 @@ export default function Home() {
               onChange={handleHours}
               className={styles.input}
             />
-            <div className={styles.answer}>{hours}</div>
+            <div className={styles.answer}>{hours} per week</div>
+          </div>
+
+          <div className={styles.questionContainer}>
+            <h2 className={styles.questionText}> Total Hours</h2>
+            <div className={styles.totalAnswer}>£{totalCost}</div>
+          </div>
+
+          <div className={styles.questionContainer}>
+            <h2 className={styles.questionText}> Select funding options</h2>
+            <select onChange={handleFunding} className={styles.dropdownMenu}>
+              <option value="2 year old, 15 hours funding">2yrs 15hrs</option>
+              <option value="3 year old, 15 hours funding">3yrs 15hrs</option>
+              <option value="3 year old, 30 hours funding">3yrs 30hrs</option>
+              <option value="No funding">NONE</option>
+            </select>
+            <div className={styles.answer}>{funding}</div>
+            <div className={styles.answer}>{fundingHours}</div>
           </div>
 
           <div className={styles.questionContainer}>
             <h2 className={styles.totalText}> Total charge per week</h2>
             <div>
-              <h3 className={styles.totalAnswer}>{`£`}</h3>
+              <h3 className={styles.totalAnswer}>£{totalAmount}</h3>
             </div>
           </div>
 

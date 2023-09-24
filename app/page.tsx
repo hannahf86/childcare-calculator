ed2e289daa66ba5a59fa133ddeaa85be76b4f2c8
hannahf86 <hannahfeehan.design@gmail.com>
2023-09-24 11:22:03 +0100

    styles improved further, better UX

--- app/page.tsx
+++ app/page.tsx
@@ -6,34 +6,32 @@ export default function Home() {
 
   const styles = {
     background: "bg-blue-300 h-screen w-screen",
-    title: "text-center text-3xl text-white py-8 mx-8",
+    title: "text-center text-3xl text-white py-8 mx-4 font-bold",
 
-    container: "bg-white mx-12 p-6 rounded-lg ",
-    questionContainer: "flex flex-col justify-center mb-6 ",
-    questionText: "text-lg text-center mb-3 ",
-    input: "m-auto px-4 py-2 rounded-lg border-none bg-gray-200 text-center w-9/12",
-    answer: 'text-right mr-8 my-4 text-lg font-bold',
-    dropdownContainer: "flex flex-col justify-center items-center",
-    dropdownMenu: 'px-4 py-2 rounded-lg mb-8',
-    totalText: 'text-xl font-bold',
-    totalAnswer: 'text-center font-bold text-xl mt-2',
+    container: "bg-white mx-6 p-6 rounded-lg ",
+    questionContainer: "flex flex-col justify-center mb-6 shadow-md",
+    questionText: "text-lg text-center mb-3 font-bold tracking-wider",
+    input: "m-auto px-4 py-2 rounded-lg border-none bg-gray-200 text-center w-5/12 mb-4",
+    answer: 'text-right text-teal-700 font-bold text-md my-4 mx-6',
+    dropdownMenu: 'px-4 py-2 rounded-lg mb-4 w-9/12 m-auto',
+    totalText: 'text-xl font-bold text-center',
+    totalAnswer: 'text-center font-bold text-xl mt-2 mb-6',
   };
 
-  // Before you have any change of adding branching in your code, initialise your effects
   const [childAge, setChildAge] = useState<number>(0);
   const [funding, setFunding] = useState<string>(" ");
   const [hours, setHours] = useState<number>(0);
   const [rate, setRate] = useState<number>(0);
 
-  // By wrapping your changes to setRate in a useEffect, you'll only update the rate if
-  // your childAge updates, rather than all the time
+  // !! DONE - By wrapping your changes to setRate in a useEffect, you'll only update the rate if
+  // !! DONE - your childAge updates, rather than all the time 
   useEffect(() => {
     if (childAge <= 2) {
       setRate(5.5);
     } else if (childAge <= 4) {
       setRate(5);
     } else {
-      setRate(1);
+      setRate(4.5);
     }
   }, [childAge]);
 
@@ -77,39 +75,38 @@ export default function Home() {
             <div className={styles.answer}>{childAge}</div>
           </div>
 
-          <div className={styles.dropdownContainer}>
+          <div className={styles.questionContainer}>
             <h2 className={styles.questionText}> Select funding options</h2>
-            <div className={styles.questionContainer}>
-              <select onChange={handleFunding} className={styles.dropdownMenu}>
-                <option value="fundingOne">2yrs 15hrs</option>
-                <option value="fundingTwo">3yrs 15hrs</option>
-                <option value="fundingThree">3yrs 30hrs</option>
-              </select>
-              {funding}
-            </div>
-
-            <div className={styles.questionContainer}>
-              <h2 className={styles.questionText}> Enter hours per week</h2>
-              <input
-                id="hours"
-                type="number"
-                name="hours"
-                value={hours}
-                onChange={handleHours}
-                className={styles.input}
-              />
-              <div className={styles.answer}>{hours}</div>
-            </div>
-
-            <div className={styles.questionContainer}>
-              <h2 className={styles.totalText}> Total charge per week</h2>
-              <div>
-                <h3 className={styles.totalAnswer}>{`£`}</h3>
-              </div>
-            </div>
-
+            <select onChange={handleFunding} className={styles.dropdownMenu}>
+              <option value="2 year old, 15 hours funding">2yrs 15hrs</option>
+              <option value="3 year old, 15 hours funding">3yrs 15hrs</option>
+              <option value="3 year old, 30 hours funding">3yrs 30hrs</option>
+              <option value="No funding">NONE</option>
+            </select>
+            <div className={styles.answer}>{funding}</div>
           </div>
-        </div >
+
+          <div className={styles.questionContainer}>
+            <h2 className={styles.questionText}> Enter hours per week</h2>
+            <input
+              id="hours"
+              type="number"
+              name="hours"
+              value={hours}
+              onChange={handleHours}
+              className={styles.input}
+            />
+            <div className={styles.answer}>{hours}</div>
+          </div>
+
+          <div className={styles.questionContainer}>
+            <h2 className={styles.totalText}> Total charge per week</h2>
+            <div>
+              <h3 className={styles.totalAnswer}>{`£`}</h3>
+            </div>
+          </div>
+
+        </div>
       </div >
 
     </>
